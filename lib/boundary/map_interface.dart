10b12c00b7f65c2e9183aef541020e4baa4c03d2
yanrenyu0 <yanrenyu00@gmail.com>
2023-03-13 01:42:13 +0800

    added markers

--- lib/boundary/map_interface.dart
+++ lib/boundary/map_interface.dart
@@ -16,6 +16,7 @@ class _MapInterfaceState extends State<MapInterface> {
   GoogleMapController? mapController;
   Timer? timer;
   String status = 'waiting';
+  Set<Marker> markersList = {};
   @override
   void initState() {
     initMapController();
@@ -29,21 +30,27 @@ class _MapInterfaceState extends State<MapInterface> {
     await MapController.updateLocationAccessPermission();
     await MapController.updateCurrentUserLocation();
 
-    //testing
-    // CarParkController cpcontroller = CarParkController();
-    // cpcontroller.getAllCarparks().then((value) {
-    //   for (var element in value) {
-    //     print(element.toJson());
-    //   }
-    //   print(CarParkList(carparks: value).toJson());
-    // });
+    //create markers based on carpark then add to markersList
+    Set<Marker> markers = {};
+    CarParkController cpcontroller = CarParkController();
+    cpcontroller.getAllCarparks().then((value) {
+      for (var element in value) {
+        Marker marker = createMarker(element);
+        markers.add(marker);
+      }
+      setState(() {
+        markersList = markers;
+      });
+    });
+    //set camera position to userlocation
     MapController.setCurrentCameraPosition(
         MapController.getCurrentUserLocation());
     setState(() {
       status = 'ready';
       print("map ready");
     });
-    // do i really need every second update of current user location?
+    // do i need every second update of current user location? probably not
+    // now is only updated when user clicks on the button
     // timer = Timer.periodic(const Duration(seconds: 1), (Timer t) async {
     //   await MapController.updateCurrentUserLocation();
     //   print("updating current user location");
@@ -114,7 +121,7 @@ class _MapInterfaceState extends State<MapInterface> {
               myLocationEnabled:
                   false, //set to isLocationAccessGranted, is set to false for now cos its flooding debug console
               myLocationButtonEnabled: false,
-              markers: markers,
+              markers: markersList,
             ),
             Padding(
               padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
@@ -148,28 +155,61 @@ class _MapInterfaceState extends State<MapInterface> {
 }
 
 //for testing
-var markers = {
-  Marker(
-      markerId: const MarkerId("marker1"),
-      position: const LatLng(1.287953, 103.851784),
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen)),
-  Marker(
-      markerId: const MarkerId("marker2"),
-      position: const LatLng(1.294444, 103.846947),
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed)),
-  Marker(
-      markerId: const MarkerId("marker3"),
-      position: const LatLng(1.282375, 103.864273),
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueYellow),
-      infoWindow: InfoWindow(
-        title: "10 / 29 lots available",
-        snippet: "click for more info",
-        onTap: () {
-          print("tapped");
-        },
-      ))
-};
+// var markers = {
+//   Marker(
+//       markerId: const MarkerId("marker1"),
+//       position: const LatLng(1.287953, 103.851784),
+//       icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen)),
+//   Marker(
+//       markerId: const MarkerId("marker2"),
+//       position: const LatLng(1.294444, 103.846947),
+//       icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed)),
+//   Marker(
+//       markerId: const MarkerId("marker3"),
+//       position: const LatLng(1.282375, 103.864273),
+//       icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueYellow),
+//       infoWindow: InfoWindow(
+//         title: "10 / 29 lots available",
+//         snippet: "click for more info",
+//         onTap: () {
+//           print("tapped");
+//         },
+//       ))
+// };
 
 //for now no custom marker widget, so its just a generic yellow/red/green pin
 //then when click will open infowindow showing available lots
 
+Marker createMarker(CarPark cp) {
+  final Map cpMap = cp.toJson();
+  final String id = cpMap["CarParkID"];
+  final double latitude =
+      double.parse((cpMap["Location"] as String).split(" ")[0]);
+  final double longitude =
+      double.parse((cpMap["Location"] as String).split(" ")[1]);
+
+  final LatLng latlng = LatLng(latitude, longitude);
+  final int availableLots = cpMap["AvailableLots"];
+
+  final hue = availableLots == 0
+      ? BitmapDescriptor.hueRed
+      : availableLots < 10
+          ? BitmapDescriptor.hueYellow
+          : BitmapDescriptor.hueGreen;
+
+  return Marker(
+      markerId: MarkerId(id),
+      position: latlng,
+      icon: BitmapDescriptor.defaultMarkerWithHue(hue),
+      infoWindow: InfoWindow(
+        title: "$availableLots lots available",
+        snippet: "click for more info",
+        onTap: () {
+          print("tapped $id");
+        },
+      ));
+}
+
+//example of carpark toJson
+//{CarParkID: Y75M, Area: , Development: BLOCK 674 YISHUN AVENUE 4, 
+//Location: 1.420176401 103.8430835, AvailableLots: 327, LotType: C, Agency: HDB}
