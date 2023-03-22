6f705b6d4475e68ed328f1b6b1d9647480184eb3
Chee Wen Zhan <wzchee88@gmail.com>
2023-03-22 13:54:00 +0800

    Changed favourites Icon

--- lib/boundary/map_interface.dart
+++ lib/boundary/map_interface.dart
@@ -102,7 +102,7 @@ class _MapInterfaceState extends State<MapInterface> {
                           builder: (context) => const FavouritesInterface()));
                 },
                 icon: const Icon(
-                  Icons.star,
+                  Icons.stars,
                   size: 36,
                 ))
           ],
