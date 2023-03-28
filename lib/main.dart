4e5b4b45205772318e9b16e57f6bbbeb75ebd35f
aty2711 <angty2711@hotmail.com>
2023-03-28 21:05:44 +0800

    slayeD
    need on tap

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MapInterface(),
+      home: SearchInterface(),
     );
   }
 }
