9969e3ac788f4c5d4dba9fc76e9fc9f0e849cdf4
Chee Wen Zhan <wzchee88@gmail.com>
2023-03-30 23:32:45 +0800

    Added all test cases without their answers yet

--- lib/test/calculatorController_test.dart
+++ lib/test/calculatorController_test.dart
@@ -23,6 +23,9 @@ import 'package:parkassist/control/calculator_controller.dart';
 import 'package:parkassist/entity/carParkList.dart';
 
 void main() {
+  // disable calculator logging because these tests will be ran many times
+  CalculatorController.logging = false;
+
   group("Simple Test : \n", () {
     test("Testing simple function", () {
       int a = 1;
@@ -55,13 +58,375 @@ void main() {
   log("         1. EQUIVALENCE CLASS TESTING");
   log("--------------------------------------------------");
 
+  int counter = 1;
+  List<DateTime> xHeader = [];
+  List<DateTime> yHeader = [];
+  List<List<double>> expectedAns = [[]];
+  group("Set 1: Invalid input / Outside Central Area / Sunday", () {
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        false,
+        DateTime(2023, 4, 3, 12, 0, 0),
+        DateTime(2023, 4, 2, 12, 0, 0),
+        -1);
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        false,
+        DateTime(2023, 4, 3, 18, 0, 0),
+        DateTime(2023, 4, 3, 21, 0, 0),
+        3.6);
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        false,
+        DateTime(2023, 4, 2, 12, 0, 0),
+        DateTime(2023, 4, 2, 15, 0, 0),
+        3.6);
+  });
+
+  group("Set 2: Central area & Same day parking & Non-Sunday", () {
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        true,
+        DateTime(2023, 4, 3, 2, 0, 0),
+        DateTime(2023, 4, 3, 5, 0, 0),
+        3.6);
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        true,
+        DateTime(2023, 4, 3, 6, 0, 0),
+        DateTime(2023, 4, 3, 18, 0, 0),
+        26.4);
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        true,
+        DateTime(2023, 4, 3, 12, 0, 0),
+        DateTime(2023, 4, 3, 15, 0, 0),
+        7.2);
+  });
+
+  group(
+      "Set 3: Central area & Start date and end date differs by one day & Start day is a Sunday",
+      () {
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        true,
+        DateTime(2023, 4, 2, 6, 0, 0),
+        DateTime(2023, 4, 3, 3, 0, 0),
+        -1); //!!
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        true,
+        DateTime(2023, 4, 2, 12, 0, 0),
+        DateTime(2023, 4, 3, 3, 0, 0),
+        -1); //!!
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        true,
+        DateTime(2023, 4, 2, 18, 0, 0),
+        DateTime(2023, 4, 3, 3, 0, 0),
+        -1); //!!
+  });
+
+  group(
+      "Set 4: Central area & Start date and end date differs by one day & End day is a Sunday",
+      () {
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        true,
+        DateTime(2023, 4, 1, 12, 0, 0),
+        DateTime(2023, 4, 2, 6, 0, 0),
+        -1); //!!
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        true,
+        DateTime(2023, 4, 1, 12, 0, 0),
+        DateTime(2023, 4, 2, 12, 0, 0),
+        -1); //!!
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        true,
+        DateTime(2023, 4, 1, 12, 0, 0),
+        DateTime(2023, 4, 2, 18, 0, 0),
+        -1); //!!
+  });
+
+  group(
+      "Set 5: Central area & Start date and end date differs by one day & None of the days are Sundays",
+      () {
+    xHeader = [
+      DateTime(2022, 4, 3, 6, 0, 0),
+      DateTime(2022, 4, 3, 12, 0, 0),
+      DateTime(2022, 4, 3, 18, 0, 0)
+    ];
+    yHeader = [
+      DateTime(2022, 4, 4, 6, 0, 0),
+      DateTime(2022, 4, 4, 12, 0, 0),
+      DateTime(2022, 4, 4, 18, 0, 0)
+    ];
+    expectedAns = [[]]; //!!
+
+    _matrixTest(CalculatorController.calculateParkingFee() as Function(void p1),
+        true, xHeader, yHeader, expectedAns);
+  });
+
+  group(
+      "Set 6: Central area & Start date and end date differs by at least two days & None of the days are Sundays",
+      () {
+    xHeader = [
+      DateTime(2022, 4, 3, 6, 0, 0),
+      DateTime(2022, 4, 3, 12, 0, 0),
+      DateTime(2022, 4, 3, 18, 0, 0)
+    ];
+    yHeader = [
+      DateTime(2022, 4, 5, 6, 0, 0),
+      DateTime(2022, 4, 5, 12, 0, 0),
+      DateTime(2022, 4, 5, 18, 0, 0)
+    ];
+    expectedAns = [[]]; //!!
+
+    _matrixTest(CalculatorController.calculateParkingFee() as Function(void p1),
+        true, xHeader, yHeader, expectedAns);
+  });
+
+  group(
+      "Set 7: Central area & Start date and end date differs by at least two days & The full parking day contains a Sunday",
+      () {
+    xHeader = [
+      DateTime(2022, 4, 1, 6, 0, 0),
+      DateTime(2022, 4, 1, 12, 0, 0),
+      DateTime(2022, 4, 1, 18, 0, 0)
+    ];
+    yHeader = [
+      DateTime(2022, 4, 3, 6, 0, 0),
+      DateTime(2022, 4, 3, 12, 0, 0),
+      DateTime(2022, 4, 3, 18, 0, 0)
+    ];
+    expectedAns = [[]]; //!!
+
+    _matrixTest(CalculatorController.calculateParkingFee() as Function(void p1),
+        true, xHeader, yHeader, expectedAns);
+  });
+
   log("--------------------------------------------------");
   log("          2. BOUNDARY VALUE TESTING");
   log("--------------------------------------------------");
 
+  counter = 1;
+  xHeader = [];
+  yHeader = [];
+  expectedAns = [[]];
+
+  group("Set 1: Invalid input / Outside Central Area / Sunday", () {
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        false,
+        DateTime(2023, 4, 3, 12, 0, 0),
+        DateTime(2023, 4, 2, 12, 0, 0),
+        -1);
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        false,
+        DateTime(2023, 4, 3, 18, 0, 0),
+        DateTime(2023, 4, 3, 21, 0, 0),
+        3.6);
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        false,
+        DateTime(2023, 4, 2, 12, 0, 0),
+        DateTime(2023, 4, 2, 15, 0, 0),
+        3.6);
+  });
+
+  group("Set 2: Central area & Same day parking & Non-Sunday", () {
+    xHeader = _generateMatrixHeaders(DateTime(2023, 4, 3), "x");
+    yHeader = _generateMatrixHeaders(DateTime(2023, 4, 3), "y");
+    expectedAns = [[]]; //!!
+
+    _matrixTest(CalculatorController.calculateParkingFee() as Function(void p1),
+        true, xHeader, yHeader, expectedAns);
+  });
+
+  group(
+      "Set 3: Central area & Start date and end date differs by one day & Start day is a Sunday",
+      () {
+    xHeader = _generateMatrixHeaders(DateTime(2023, 4, 2), "x");
+    DateTime endDate = DateTime(2023, 4, 3, 3, 0, 0);
+    List<double> expected1DAns = []; //!!
+
+    counter = 1;
+    for (int i = 0; i < xHeader.length; i++) {
+      _singleTest(
+          counter++,
+          CalculatorController.calculateParkingFee() as Function(void p1),
+          true,
+          xHeader[i],
+          endDate,
+          expected1DAns[i]);
+    }
+  });
+
+  group(
+      "Set 4: Central area & Start date and end date differs by one day & End day is a Sunday",
+      () {
+    DateTime startDate = DateTime(2023, 4, 3, 3, 0, 0);
+    List<DateTime> yHeader = _generateMatrixHeaders(DateTime(2023, 4, 2), "y");
+    List<double> expected1DAns = []; //!!
+
+    counter = 1;
+    for (int i = 0; i < xHeader.length; i++) {
+      _singleTest(
+          counter++,
+          CalculatorController.calculateParkingFee() as Function(void p1),
+          true,
+          startDate,
+          yHeader[i],
+          expected1DAns[i]);
+    }
+  });
+
+  group(
+      "Set 5: Central area & Start date and end date differs by one day & None of the days are Sundays",
+      () {
+    xHeader = _generateMatrixHeaders(DateTime(2023, 4, 3), "x");
+    yHeader = _generateMatrixHeaders(DateTime(2023, 4, 4), "y");
+    expectedAns = [[]]; //!!
+
+    _matrixTest(CalculatorController.calculateParkingFee() as Function(void p1),
+        true, xHeader, yHeader, expectedAns);
+  });
+
+  group(
+      "Set 6: Central area & Start date and end date differs by at least two days & None of the days are Sundays",
+      () {
+    xHeader = _generateMatrixHeaders(DateTime(2023, 4, 3), "x");
+    yHeader = _generateMatrixHeaders(DateTime(2023, 4, 5), "y");
+    expectedAns = [[]]; //!!
+
+    _matrixTest(CalculatorController.calculateParkingFee() as Function(void p1),
+        true, xHeader, yHeader, expectedAns);
+  });
+
+  group(
+      "Set 7: Central area & Start date and end date differs by at least two days & The full parking day contains a Sunday",
+      () {
+    xHeader = _generateMatrixHeaders(DateTime(2023, 4, 1), "x");
+    yHeader = _generateMatrixHeaders(DateTime(2023, 4, 3), "y");
+    expectedAns = [[]]; //!!
+
+    _matrixTest(CalculatorController.calculateParkingFee() as Function(void p1),
+        true, xHeader, yHeader, expectedAns);
+  });
+
   log("--------------------------------------------------");
   log("           3. CONTROL FLOW TESTING");
   log("--------------------------------------------------");
+
+  group("Testing calculatorController.calculateParkingFees():", () {
+    int counter = 1;
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        false,
+        DateTime(2023, 4, 3, 12, 0, 0),
+        DateTime(2023, 4, 2, 12, 0, 0),
+        -1);
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        false,
+        DateTime(2023, 4, 3, 18, 0, 0),
+        DateTime(2023, 4, 3, 21, 0, 0),
+        3.6);
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        false,
+        DateTime(2023, 4, 3, 23, 0, 0),
+        DateTime(2023, 4, 4, 2, 0, 0),
+        -1);
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        true,
+        DateTime(2023, 4, 3, 23, 0, 0),
+        DateTime(2023, 4, 5, 2, 0, 0),
+        44.4);
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        false,
+        DateTime(2023, 4, 3, 23, 0, 0),
+        DateTime(2023, 4, 4, 2, 0, 0),
+        32.4);
+  });
+
+  group("Testing CalculatorController._calculateCentralCarpark():", () {
+    int counter = 1;
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        false,
+        DateTime(2023, 4, 3, 12, 0, 0),
+        DateTime(2023, 4, 3, 15, 0, 0),
+        3.6);
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        true,
+        DateTime(2023, 4, 3, 18, 0, 0),
+        DateTime(2023, 4, 3, 21, 0, 0),
+        3.6);
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        true,
+        DateTime(2023, 4, 3, 3, 0, 0),
+        DateTime(2023, 4, 3, 6, 0, 0),
+        3.6);
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        true,
+        DateTime(2023, 4, 3, 5, 30, 0),
+        DateTime(2023, 4, 3, 8, 30, 0),
+        5.4);
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        true,
+        DateTime(2023, 4, 3, 6, 0, 0),
+        DateTime(2023, 4, 3, 18, 0, 0),
+        26.4);
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        true,
+        DateTime(2023, 4, 3, 12, 0, 0),
+        DateTime(2023, 4, 3, 15, 0, 0),
+        7.2);
+    _singleTest(
+        counter++,
+        CalculatorController.calculateParkingFee as Function(void p1),
+        false,
+        DateTime(2023, 4, 3, 15, 30, 0),
+        DateTime(2023, 4, 3, 18, 30, 0),
+        5.4);
+  });
 }
 
 void testThis(Function(void) targetMethod, bool testIsMatrix, bool isCentral,
@@ -93,8 +458,7 @@ void testThis(Function(void) targetMethod, bool testIsMatrix, bool isCentral,
       formattedAns[i] = row;
     }
 
-    matrixTest(
-        targetMethod, testIsMatrix, isCentral, xHeader, yHeader, formattedAns);
+    _matrixTest(targetMethod, isCentral, xHeader, yHeader, formattedAns);
   } else {
     // check if args format is correct or not
 
@@ -102,13 +466,12 @@ void testThis(Function(void) targetMethod, bool testIsMatrix, bool isCentral,
     DateTime end = args[1];
     double formattedAns = expectedAns[0];
 
-    singleTest(targetMethod, testIsMatrix, isCentral, start, end, formattedAns);
+    _singleTest(1, targetMethod, isCentral, start, end, formattedAns);
   }
 }
 
-void matrixTest(
+void _matrixTest(
     Function(void) targetMethod,
-    bool testIsMatrix,
     bool isCentral,
     List<DateTime> xHeader,
     List<DateTime> yHeader,
@@ -119,10 +482,6 @@ void matrixTest(
     CalculatorController.carpark.area = "";
   }
 
-  test("Matrix Test \t x=? y=? \t Start date time: \t End date time:", () {
-    expect(Function.apply(targetMethod, []), 9999999999999);
-  });
-
   // Loop check
   if (xHeader.length != expectedAns.length) {
     log("Number of elements in the x-axis does not match the expected list!");
@@ -131,12 +490,13 @@ void matrixTest(
     log("Number of elements in the y-axis does not match the expected list!");
   }
 
+  int counter = 1;
   for (int i = 0; i < xHeader.length; i++) {
     for (int j = 0; j < yHeader.length; j++) {
       CalculatorController.setStartDateTime(xHeader[i]);
       CalculatorController.setEndDateTime(yHeader[j]);
       test(
-          "Start: (x=$i) ${xHeader[i].toString()} \t End: (y=$j) ${yHeader[i].toString()} \t Expected: ${expectedAns[i][j]}",
+          "${counter++} Start: (x=$i) ${xHeader[i].toString()} \t End: (y=$j) ${yHeader[i].toString()} \t Expected: ${expectedAns[i][j]}",
           () {
         expect(Function.apply(targetMethod, []), expectedAns[i][j]);
       });
@@ -144,7 +504,7 @@ void matrixTest(
   }
 }
 
-void singleTest(Function(void) targetMethod, bool testIsMatrix, bool isCentral,
+void _singleTest(int counter, Function(void) targetMethod, bool isCentral,
     DateTime start, DateTime end, double expectedAns) {
   if (isCentral) {
     CalculatorController.carpark.area = "BBB";
@@ -154,7 +514,33 @@ void singleTest(Function(void) targetMethod, bool testIsMatrix, bool isCentral,
   CalculatorController.setStartDateTime(start);
   CalculatorController.setEndDateTime(end);
 
-  test("Start: $start \t End: $end \t Expected: $expectedAns", () {
+  test("${counter++} Start: $start \t End: $end \t Expected: $expectedAns", () {
     expect(Function.apply(targetMethod, []), expectedAns);
   });
+}
+
+List<DateTime> _generateMatrixHeaders(DateTime date, String axis) {
+  List<DateTime> headerList = [];
+
+  if (axis == 'x') {
+    headerList.add(DateTime(date.year, date.month, date.day, 0, 0, 0));
+    headerList.add(DateTime(date.year, date.month, date.day, 1, 0, 0));
+    headerList.add(DateTime(date.year, date.month, date.day, 6, 0, 0));
+    headerList.add(DateTime(date.year, date.month, date.day, 7, 0, 0));
+    headerList.add(DateTime(date.year, date.month, date.day, 8, 0, 0));
+    headerList.add(DateTime(date.year, date.month, date.day, 16, 0, 0));
+    headerList.add(DateTime(date.year, date.month, date.day, 17, 0, 0));
+    headerList.add(DateTime(date.year, date.month, date.day, 18, 0, 0));
+  } else if (axis == 'y') {
+    headerList.add(DateTime(date.year, date.month, date.day, 6, 0, 0));
+    headerList.add(DateTime(date.year, date.month, date.day, 7, 0, 0));
+    headerList.add(DateTime(date.year, date.month, date.day, 8, 0, 0));
+    headerList.add(DateTime(date.year, date.month, date.day, 16, 0, 0));
+    headerList.add(DateTime(date.year, date.month, date.day, 17, 0, 0));
+    headerList.add(DateTime(date.year, date.month, date.day, 18, 0, 0));
+    headerList.add(DateTime(date.year, date.month, date.day, 23, 0, 0));
+    headerList.add(DateTime(date.year, date.month, date.day + 1, 0, 0, 0));
+  }
+
+  return headerList;
 }
